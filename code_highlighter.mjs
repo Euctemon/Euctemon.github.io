9cd260b884745cd8ea52009ecdd8dfa5b3083bd0
David Vodička <81967103+Euctemon@users.noreply.github.com>
2024-08-07 21:05:26 +0200

    New page

--- code_highlighter.mjs
+++ code_highlighter.mjs
@@ -2,33 +2,28 @@ import { codeToHtml } from 'shiki'
 
 const code =
 `
-let rec simp_bool e = match e with
+let rec to_cnf_type (e : prop) : comp_prop = match e with
+  | Const a -> Const a
+  | Var a -> Var a
+  | Not a -> Not (to_cnf_type a)
+  | And (a,b) -> And (to_cnf_type a, to_cnf_type b)
+  | Or (a,b) -> Or (to_cnf_type a, to_cnf_type b)
+  | Impl (a,b) -> Or (Not (to_cnf_type a), to_cnf_type b)
+  | Equiv (a,b) -> And (to_cnf_type (Impl (a,b)),to_cnf_type (Impl (b,a)))`
+
+
+const foo =
+`
+
+let rec to_cnf_type (e : prop) : comp_prop = match e with
 | Const a -> Const a
-| Var v -> Var v
-| Not m ->
-  begin match simp_bool m with
-  | Not (Const c) -> Const c
-  | Const c -> Const (not c)
-  | e -> Not e
-  end
-| Or (a,b) -> begin let or_res = (simp_bool a, simp_bool b) in
-  match or_res with
-  | (Const true,_) -> Const true
-  | (_,Const true) -> Const true
-  | (Const false, Const false) -> Const false
-  | (Const false,_) -> snd or_res
-  | (_,Const false) -> fst or_res
-  | _ -> Or (fst or_res, snd or_res)
-  end
-| And (a,b) -> begin let and_res = (simp_bool a, simp_bool b) in
-  match and_res with
-  | (Const true,Const true) -> Const true
-  | (Const false,_) -> Const false
-  | (_,Const false) -> Const false
-  | (Const true,_) -> snd and_res
-  | (_,Const true) -> fst and_res
-  | _ -> And (fst and_res, snd and_res)
-  end`
+| Var a -> Var a
+| Not a -> Not (to_cnf_type a)
+| And (a,b) -> And (to_cnf_type a, to_cnf_type b)
+| Or (a,b) -> Or (to_cnf_type a, to_cnf_type b)
+| Impl (a,b) -> Or (Not (to_cnf_type a), to_cnf_type b)
+| Equiv (a,b) -> And (to_cnf_type (Impl (a,b)),to_cnf_type (Impl (b,a)))
+`
 
 const html = await codeToHtml (code, { lang: 'ocaml', theme: 'github-light'})
 
--- styles/personal_scripts.js
+++ styles/personal_scripts.js
@@ -2,7 +2,7 @@ function openMenu(id1,id2) {
   var elem = document.getElementById(id1);
   var svg_img = document.getElementById(id2);
 
-if (svg_img.src.match("../styles/sidebarOpen.svg")) {
+if (svg_img.src.match("../styles/sidebar_open.svg")) {
   svg_img.src="../styles/sidebar_close.svg";
   elem.style.left = '0px';
   }
