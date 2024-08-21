550d2dc860cfc2460994202975704211ce8b371e
David Vodička <81967103+Euctemon@users.noreply.github.com>
2024-08-21 19:58:57 +0200

    RAA intro

--- foo.hs
+++ foo.hs
@@ -1,8 +1,22 @@
 data Prop = Const Bool | Var Int | Neg Prop | And Prop Prop | Or Prop Prop | Imp Prop Prop
 
+newtype Context = Context [Prop]
 
 
+addContext (Context a) (Context b) = Context (a ++ b)
 
 
-bobo = Or (Const True) (Var 2)
+data Entails = Entails Context Prop
+newtype Proof = Proof Entails
+
+assumptionRule a = Entails (Context [a]) a
+
+
+conjI (Entails r p) (Entails s q) = Entails (addContext r s) (And p q)
+
+conjEL (Entails r (And a b)) = Entails r a
+conjER (Entails r (And a b)) = Entails r b
+
+
+m a b = Proof (let h = conjI (assumptionRule a) (assumptionRule b) in conjI (conjER h) (conjEL h))
 
