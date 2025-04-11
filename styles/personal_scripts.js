d9cb435665f454abbc0a5cd42fb6b55f79a84451
Euctemon <dave@vodickovi.cz>
2025-04-11 15:58:34 +0200

    Přidání menu

--- styles/personal_scripts.js
+++ styles/personal_scripts.js
@@ -1,13 +1,8 @@
-function openMenu(id1,id2) {
-  var elem = document.getElementById(id1);
-  var svg_img = document.getElementById(id2);
-
-if (svg_img.src.match("../styles/sidebar_open.svg")) {
-  svg_img.src="../styles/sidebar_close.svg";
-  elem.style.left = '0px';
-  }
-else {
-  elem.style.left = '-200%';
-  svg_img.src="../styles/sidebar_open.svg";
+function myFunction() {
+  var x = document.getElementById("myDIV");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
   }
 }
