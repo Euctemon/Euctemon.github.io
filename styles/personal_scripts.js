ba30550fb3f20cff9f917262c111d770174f6de8
David Vodička <dave@vodickovi.cz>
2025-04-11 17:34:03 +0200

    Oprava zarovnání hlavních stránek

--- styles/personal_scripts.js
+++ styles/personal_scripts.js
@@ -1,5 +1,13 @@
-function myFunction() {
-  var x = document.getElementById("myDIV");
+window.onscroll = function() {
+  document.getElementById("blog-nav").style.display = "none";
+};
+
+window.onload = function() {
+  document.getElementById("blog-nav").style.display = "none";
+};
+
+function navOpener() {
+  var x = document.getElementById("blog-nav");
   if (x.style.display === "none") {
     x.style.display = "block";
   } else {
